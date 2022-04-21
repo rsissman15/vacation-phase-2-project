57806388d34633dc1b3caa57a6a30b19d9698361
Rebecca Sissman <rsissman15@aol.com>
2022-04-21 12:54:55 -0400

    Created Client Router

---
+++ src/components/Vacations/ListVacations.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListVacations = () => {
+  return (
+    <div>
+        <h1>List Vacations</h1>
+    </div>
+  )
+}
+
+export default ListVacations
---
+++ src/components/Vacations/NewVacation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewVacation = () => {
+  return (
+    <div>
+      <h1>New Vacation</h1>
+    </div>
+  )
+}
+
+export default NewVacation
