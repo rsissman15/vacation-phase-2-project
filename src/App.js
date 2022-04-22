aac99222b16d4560ef81586b5b826ad15724775d
Rebecca Sissman <rsissman15@aol.com>
2022-04-22 15:11:37 -0400

    updated db.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React,{useState,useEffect} from 'react'
 import Home from './components/Home';
 import NavBar from './components/Navbar';
-import ListVacations from './components/Vacations/ListVacations';
-import NewVacation from './components/Vacations/NewVacation';
+import MainContainer from './components/MainContainer';
+import NewVacation from './components/Vacations/NewVacationForm';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
+
 const App=()=> {
-  const [vacations,setVacations]=useState([])
-
-  useEffect(()=>{
-
-    const fetchVacations=()=>{
-      fetch("http://localhost:3001/vacations")
-      .then(res=>res.json())
-      .then(data=>setVacations(data))
-    }
-
-    fetchVacations();
-
-  },[])
-
+  
   
 
   return (
@@ -27,7 +15,7 @@ const App=()=> {
       <NavBar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/vacations" element={<ListVacations vacations={vacations}/>}/>
+        <Route path="/vacations" element={<MainContainer/>}/>
         <Route path="/vacations/new" element={<NewVacation/>}/>
       </Routes>
       
