fd8fa1debd2a5fab251f65bcbcea1d0520ea5309
Rebecca Sissman <rsissman15@aol.com>
2022-05-02 10:41:27 -0400

    added state to app instead of main container

--- src/App.js
+++ src/App.js
@@ -7,16 +7,77 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 
 const App=()=> {
-  
-  
+    const[vacations,setVacations]=useState([])
+    const[sortVacations,setSortVacations]=useState('')
+    const[filter,setFilter]=useState('All')    
+
+  useEffect(()=>{
+
+      fetch("http://localhost:8000/vacations")
+      .then(res=>res.json())
+      .then(data=>setVacations(data))
+  },[])
+
+
+  function addVacation(vacation){
+    setVacations([...vacations,vacation])
+
+}
+
+  const displayVacations=()=>{
+    if(filter==='All'){
+           if(sortVacations==="Alphabetically"){
+            return [...vacations].sort((a,b)=> a.country > b.country ? 1 : -1)
+           }
+           return [...vacations]
+
+    }
+    else{
+      const filterVacations=[...vacations].filter(vacation=>vacation.continent===filter)
+        if(sortVacations==="Alphabetically"){
+         return filterVacations.sort((a,b)=> a.country > b.country ? 1 : -1)
+        }
+        return filterVacations
+
+    }
+    // if(filter!=="All"){
+    //     const filterVacations=[...vacations].filter(vacation=>vacation.continent===filter)
+    //     if(sortVacations==="Alphabetically"){
+    //      return filterVacations.sort((a,b)=> a.country > b.country ? 1 : -1)
+    //     }
+    //     return filterVacations
+    // }
+    // else{
+    //     if(sortVacations==="Alphabetically"){
+    //         return [...vacations].sort((a,b)=> a.country > b.country ? 1 : -1)
+    //        }
+    //        return [...vacations]
+    // }
+  }
+
+
+const updateSort = (vacation) => {
+  setSortVacations(vacation);
+};
+
+
+const updateFilter=(vacation)=>{
+  setFilter(vacation)
+}
+
 
   return (
     <Router>
       <NavBar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/vacations" element={<MainContainer/>}/>
-        <Route path="/vacations/new" element={<NewVacation/>}/>
+        <Route path="/vacations/new" element={<NewVacation addVacation={addVacation}/>}/>
+        <Route path="/vacations" element={<MainContainer 
+        vacations={displayVacations()} 
+        updateFilter={updateFilter}
+        updateSort={updateSort}
+        setSortVacations={setSortVacations} 
+        setFilter={setFilter}/>}/>
       </Routes>
       
     </Router>
